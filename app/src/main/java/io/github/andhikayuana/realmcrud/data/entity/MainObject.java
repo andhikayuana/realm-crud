3d681e66a41be91cb3bf8e2c007a039420f3a6c1
Andhika Yuana <andhikayuana@gmail.com>
2018-01-29 22:03:37 +0700

    update primitive support

---
+++ app/src/main/java/io/github/andhikayuana/realmcrud/data/entity/MainObject.java
@@ -0,0 +1,11 @@
+package io.github.andhikayuana.realmcrud.data.entity;
+
+import io.realm.RealmObject;
+
+/**
+ * @author yuana <andhikayuana@gmail.com>
+ * @since 7/18/17
+ */
+
+public class MainObject extends RealmObject {
+}
---
+++ app/src/main/java/io/github/andhikayuana/realmcrud/data/entity/RealmInt.java
@@ -0,0 +1,9 @@
+package io.github.andhikayuana.realmcrud.data.entity;
+
+/**
+ * @author yuana <andhikayuana@gmail.com>
+ * @since 7/18/17
+ */
+
+class RealmInt {
+}
